5acdca4118636cfbb2a6b4121bf0ab8ecc9fc9d0
josh <joshjreed@gmail.com>
2026-09-14 17:03:28 -0600

    tests

--- src/features/about/about-counter.ts
+++ src/features/about/about-counter.ts
@@ -4,6 +4,7 @@ defineWebComponent("about-counter", (c) => {
   return c.defineState({
     count: 0,
   })
+    .defineShadow(false)
     .defineObservedAttributes({
       name: "World",
     })
@@ -18,7 +19,7 @@ defineWebComponent("about-counter", (c) => {
         <div>
           <p>Hello, ${name}!</p>
           <p>Count: ${count}</p>
-          <button @click=${el.increment}>Increment</button>
+          <button class="button primary" @click=${el.increment}>Increment</button>
           ${count >= 10 ? html`<p>You've reached 10!</p>` : ""}
         </div>
       `;
