824dc0a7e4c18a7dde310fa4206e7ba598a5c39c
josh <joshjreed@gmail.com>
2026-09-14 17:12:53 -0600

    updates

--- src/features/about/about-counter.ts
+++ src/features/about/about-counter.ts
@@ -6,22 +6,20 @@ defineWebComponent("about-counter", (c) => {
   })
     .defineShadow(false)
     .defineObservedAttributes({
-      name: "World",
+      name: "John",
+      lastName: "Smith",
     })
     .defineMethod("increment", (element) => {
       return () => element.state.count++;
     })
     .defineRender((el) => {
-      const { count } = el.state;
-      const { name } = el.observedAttribute;
+      const fullname = `${el.observedAttribute.name} ${el.observedAttribute.lastName}`;
 
       return html`
-        <div>
-          <p>Hello, ${name}!</p>
-          <p>Count: ${count}</p>
-          <button class="button primary" @click=${el.increment}>Increment</button>
-          ${count >= 10 ? html`<p>You've reached 10!</p>` : ""}
-        </div>
+        <p>Hello, ${fullname}!</p>
+        <p>Count: ${el.state.count}</p>
+        <button class="button primary" @click=${el.increment}>Increment</button>
+        ${el.state.count >= 10 ? html`<p>You've reached 10!</p>` : ""}
       `;
     });
 });
